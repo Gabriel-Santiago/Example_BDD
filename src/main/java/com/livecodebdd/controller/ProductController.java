b3037f5de2b7acd7496e657c2f9c20e1be087a13
Gabriel <gabrielsrmj@alu.ufc.br>
2024-01-30 09:20:47 -0300

    Verificação

--- src/main/java/com/livecodebdd/controller/ProductController.java
+++ src/main/java/com/livecodebdd/controller/ProductController.java
@@ -1,6 +1,6 @@
 package com.livecodebdd.controller;
 
-
+//import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
